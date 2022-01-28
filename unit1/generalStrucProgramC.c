ebc373ea4db0dccda1242377b7dff9bc4c2882e2
BriannaBalamV <2109013@upy.edu.mx>
2022-01-28 08:40:23 -0600

    estructura general C

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//*Global Variables *//;
+//Create a variable: dataType nameVariable = anyData;
+//dataTypes in C/C++/C#/PHP
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "BriannaAyelen";
+char unCaracter = 'F';
+///////////////////////////////////
+
+
+// La ejecucion del programa//
+int main(){
+    printf("Hello Data B %s\n", name);
+    return 0;
+}
+//////////////////////////////////
