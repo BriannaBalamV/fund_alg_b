0c106f5ae35d826a3dea0a20a5316497dfaa49b0
BriannaBalamV <2109013@upy.edu.mx>
2022-02-09 18:44:13 -0600

    argv lesson

--- unit1/variablesGlobalLocal.c
+++ unit1/variablesGlobalLocal.c
@@ -8,9 +8,13 @@ char* date = "02-02-22T10:25:00z";
 // La ejecucion del programa//
 int main(){
     date = "02-03-22T10:25:00z";
-    int hours = 22; //Local:main
+    int hours = 22; //Local:main %d - %i
+    float pi= 3.1416; // %f
+    char character= 'c'; // %c
+    char* name= "brianaa"; // %s
+    int *address = &hours; // %p 
     hours = hours +2;
-    printf("hours %d\n, hours");
+    printf("hours: %d, %s\n", hours, name);
     return 0;
 }
 //////////////////////////////////
